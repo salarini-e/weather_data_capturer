9acd52604f7e3de07bf2add93b38ec9bd6f44c33
Enzzo Cavallo <souenzzo@gmail.com>
2024-04-17 19:17:19 -0300

    Add venv setup

--- weather_data_capturer/settings.py
+++ weather_data_capturer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-68((!5k4h-hf8l&kwms7v_p0p)^qms-6&0unm4r93u=)z^%$7r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vps52605.publiccloud.com.br', 'cdm.esalarini.com.br']
+ALLOWED_HOSTS = ['vps52605.publiccloud.com.br', 'cdm.esalarini.com.br', '127.0.0.1']
 
 
 # Application definition
